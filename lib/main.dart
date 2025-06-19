c08aa85dea41f21d2c679a0a8cc55bd4ce8bce7c
Gian132004 <giannicolaiduhan@gmail.com>
2025-06-19 10:06:51 +0800

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,109 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Engine CC Calculator', style: TextStyle(color: Colors.white,
-        fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.red
+        title: Text(
+          'Engine CC Calculator',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-          child: Column( mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
-        children:  [ Text('Alam mo ha!!!', style: TextStyle(color: Colors.white ,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            decoration: TextDecoration.underline,
-            fontStyle: FontStyle.italic,
-            background: Paint()
-            .. style = PaintingStyle.fill
-            .. color = Colors.red
-        )), Text('Alam mo ha!!!', style: TextStyle(color: Colors.white ,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            decoration: TextDecoration.underline,
-            fontStyle: FontStyle.italic,
-            background: Paint()
-              .. style = PaintingStyle.fill
-              .. color = Colors.red
-        )), Text('Alam mo ha!!!', style: TextStyle(color: Colors.white ,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            decoration: TextDecoration.underline,
-            fontStyle: FontStyle.italic,
-            background: Paint()
-              .. style = PaintingStyle.fill
-              .. color = Colors.red
-        )),
-        ]
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    'Name:',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    'Kupong',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    'Age:',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    '20',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(5, 4, 3, 2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    'Gender:',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    'Male',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     ),
   ));
 }
-
